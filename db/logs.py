26d807a73b0fb9b66c3915a1092d522fd130aff2
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2018-11-28 12:30:33 +0000

    draft of log and users interface

---
+++ db/logs.py
@@ -0,0 +1,21 @@
+class logs:
+    def __init__(self):
+        pass
+
+    def store_move(self, ist_id, latitude, longitude):
+        pass
+
+    def store_msg_user(self, ist_id, msg):
+        pass
+
+    def store_msg_building(self, bid, msg):
+        pass
+
+    def get_moves(self, ist_id):
+        pass
+
+    def get_msgs_user(self, ist_id):
+        pass
+
+    def get_msgs_building(self, bid):
+        pass
---
+++ db/user.py
@@ -0,0 +1,28 @@
+class users:
+    def __init__(self):
+        pass
+
+    def check_in(self, ist_id):
+        pass
+
+    def check_out(self, ist_id):
+        pass
+
+    def send_msg(self, ist_id, msg):
+        pass
+
+    def get_msgs(self, ist_id):
+        # return [msg] list
+        pass
+
+    def set_postion(self, ist_id, latitude, longitude):
+        pass
+
+    def get_position(self, ist_id):
+        # return (latitude, longitude) if checked in, null otherwise
+        pass
+
+    def find_close_users(self, ist_id):
+        # return [users] list if checked in, null otherwise
+        pass
+
