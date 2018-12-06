023fb41ac4ba6d33d79d5065380afed99ec0bb4e
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2018-12-06 19:49:37 +0000

    started connection to database

--- DbClient/db.py
+++ DbClient/db.py
@@ -3,7 +3,12 @@ import sqlite3
 from Utils.private_consts import DbAccess
 
 def init_db(db):
-    db.
+    qry = open('DbClient/schema.sql', 'r').read()
+    sqlite3.complete_statement(qry)
+    cursor = db.cursor()
+
+    try:
+        cursor.executescript(qry)
 
 def get_db():
     db = sqlite3.connect(DbAccess.DB_NAME)
@@ -11,5 +16,5 @@ def get_db():
     return db
 
 def close_db(db):
-    db.
+    db.close()
 
--- DbClient/schema.sql
+++ DbClient/schema.sql
@@ -11,21 +11,36 @@ CREATE TABLE building (
     PRIMARY KEY (id)
 );
 
-create TABLE ist_user (
+CREATE TABLE ist_user (
     ist_ID VARCHAR(20),
-    token VARCHAR(20) UNIQUE,
     latitude NUMERIC(6, 3),
     longitude NUMERIC(6, 3),
     radius NUMERIC(6, 3),
-    cur_building id,
+    cur_building INTEGER,
     PRIMARY KEY (ist_ID),
     FOREIGN KEY (cur_building) REFERENCES building(id)
 );
 
-create TABLE robot (
-    id INTEGER,
+CREATE TABLE bot (
+    id INTEGER NOT NULL AUTO_INCREMENT,
     token VARCHAR(20) UNIQUE NOT NULL,
     building INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (building) REFERENCES building(id)
+);
+
+CREATE TABLE message_user (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    ist_ID VARCHAR(20),
+    message TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (ist_ID) REFERENCES ist_user(ist_ID)
+);
+
+CREATE TABLE message_building (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    building INTEGER,
+    message TEXT,
     PRIMARY KEY (id),
     FOREIGN KEY (building) REFERENCES building(id)
 );
